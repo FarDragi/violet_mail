6ac62b92511e76c44991310976bef328a0ed9c24
Takasakiii <lucasmc2709@live.com>
2021-06-08 22:49:01 -0300

    static manager

--- src/client.rs
+++ src/client.rs
@@ -1,95 +1,57 @@
-use std::{error::Error, thread::{self, spawn}};
+pub type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
+pub type GResult<T> = Result<T, GenericError>;
 
-use isahc::{AsyncBody, HttpClient, Response};
-use log::{Level, Log, Metadata, Record, info, set_logger, set_max_level};
-use serde_json::to_string;
+pub(self) mod static_data {
+    use lazy_static::lazy_static;
+    use std::{any::Any, collections::HashMap, sync::Mutex};
 
-use crate::log::{VioletLog, VioletLogSeverity};
+    use super::GResult;
 
-pub struct VioletMail {
-    base_url: String,
-    client: HttpClient
-}
-
-static mut VIOLET_INITIALIZED: bool = false;
-
-pub type VioletError = Box<dyn Error + Send + Sync + 'static>;
-
-impl VioletMail {
-    pub fn init(identifier: u32, token: &str) -> Result<&'static Self, VioletError> {
-        unsafe {
-            if !VIOLET_INITIALIZED {
-                let client = HttpClient::builder()
-                    .default_header("Authorization", token)
-                    .default_header("Content-Type", "application/json")
-                    .build()?;
-        
-                let base_url = format!("https://violet.zuraaa.com/api/apps/{}/events", identifier);
-        
-                let violet_mail = Box::new(Self {
-                    client,
-                    base_url
-                });
-        
-                let violet_leak: &'static VioletMail = Box::leak(violet_mail); 
-        
-                set_logger(violet_leak).map_err(|err|
-                    format!("{:?}", err)
-                )?;
-                set_max_level(log::LevelFilter::Trace);
-        
-                Ok(violet_leak)
-            } else {
-                Err("Violet mail ja iniciado".into())
-            }
-        }
+    lazy_static! {
+        static ref STORAGE_STATIC: Mutex<StaticData> = StaticData::new();
     }
 
-    pub fn send_log(&self, violet_log: &VioletLog) {
-        let body = to_string(violet_log);
+    pub struct StaticData {
+        data: HashMap<String, Box<dyn Any + Send + Sync>>,
+    }
 
-        let client = &self.client;
-        let base_url = &self.base_url;
-
-        let handle = spawn(move || {
-            let future = async {
-                client.post_async(base_url, body.unwrap()).await
+    impl StaticData {
+        fn new() -> Mutex<Self> {
+            let data = Self {
+                data: HashMap::new(),
             };
 
-            futures::executor::block_on(future)
-        });
+            Mutex::new(data)
+        }
 
+        fn set_data_value(&mut self, name: String, data: impl Any + Send + Sync) {
+            if let Some(mapped) = self.data.get_mut(&name) {
+                *mapped = Box::new(data);
+            } else {
+                self.data.insert(name, Box::new(data));
+            }
+        }
+
+        pub fn add_element_to_static(name: String, data: impl Any + Send + Sync) -> GResult<()> {
+            let mut locked_data = STORAGE_STATIC.lock().map_err(|why| format!("{:?}", why))?;
+            locked_data.set_data_value(name, data);
+            Ok(())
+        }
+
+        pub fn get_data_elemet_static<T: 'static, F>(name: &str, callback: F) -> GResult<()>
+        where
+            F: Fn(&T),
+        {
+            let locked_data = STORAGE_STATIC.lock().map_err(|why| format!("{:?}", why))?;
+            let data = locked_data
+                .data
+                .get(name)
+                .ok_or("Key não possui dados")?
+                .downcast_ref::<T>()
+                .ok_or("Downcast não pode inferir esse tipo")?;
+
+            callback(data);
+            Ok(())
+        }
     }
 }
-
-impl Log for VioletMail {
-    fn enabled(&self, _: &Metadata) -> bool {
-        true
-    }
-
-    fn log(&self, record: &Record) {
-        println!("{} - {}", record.level(), record.args());
-
-        let violet_log = VioletLog::new(
-            VioletLogSeverity::from(record.level()), 
-            "Violet mail".to_string(), 
-            record.args().to_string()
-        );
-
-        self.send_log(&violet_log);
-    }
-
-    fn flush(&self) {
-        todo!()
-    }
-}
-
-// pub trait SendToVioletMail {
-//     fn send_to_violet();
-// }
-
-// impl SendToVioletMail for dyn Error {
-//     fn send_to_violet() {
-//         todo!()
-//     }
-// }
