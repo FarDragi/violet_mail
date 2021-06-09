83d7f70fba88b93fe00f99fe7b07e50126eb5579
Takasakiii <lucasmc2709@live.com>
2021-06-09 02:02:15 -0300

    por hj eh isso grande dia

--- src/client.rs
+++ src/client.rs
@@ -1,57 +1,88 @@
+use std::{thread, time::Duration};
+
+use isahc::{config::Configurable, Request, RequestExt};
+
+use log::{Metadata, Record};
+
+use crate::prelude::{VioletLog, VioletLogSeverity};
 pub type GenericError = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub type GResult<T> = Result<T, GenericError>;
 
-pub(self) mod static_data {
-    use lazy_static::lazy_static;
-    use std::{any::Any, collections::HashMap, sync::Mutex};
+#[derive(Clone)]
+struct HttpVioletData {
+    indetifier: u64,
+    token: String,
+}
 
-    use super::GResult;
-
-    lazy_static! {
-        static ref STORAGE_STATIC: Mutex<StaticData> = StaticData::new();
+impl HttpVioletData {
+    fn new(indetifier: u64, token: String) -> Self {
+        Self { indetifier, token }
     }
 
-    pub struct StaticData {
-        data: HashMap<String, Box<dyn Any + Send + Sync>>,
+    async fn send_data(
+        &self,
+        title: String,
+        severity: VioletLogSeverity,
+        message: String,
+    ) -> GResult<()> {
+        let log_vio = VioletLog::new(severity, title, message);
+        let log_vio_json = serde_json::to_string(&log_vio)?;
+
+        let retorno = Request::post(format!(
+            "https://violet.zuraaa.com/api/apps/{}/events",
+            self.indetifier
+        ))
+        .header("Content-Type", "application/json")
+        .header("Authorization", &self.token)
+        .timeout(Duration::from_secs(20))
+        .body(log_vio_json)?
+        .send_async()
+        .await?;
+        let body = retorno.status();
+        println!("{:?}", body);
+        Ok(())
+    }
+}
+
+pub fn init(indentifier: u64, token: String) {
+    static mut HAS_INIT: bool = false;
+
+    unsafe {
+        if HAS_INIT {
+            return;
+        }
     }
 
-    impl StaticData {
-        fn new() -> Mutex<Self> {
-            let data = Self {
-                data: HashMap::new(),
-            };
+    let leak_content: &'static mut HttpVioletData =
+        Box::leak(Box::new(HttpVioletData::new(indentifier, token)));
 
-            Mutex::new(data)
-        }
+    log::set_logger(leak_content).unwrap();
 
-        fn set_data_value(&mut self, name: String, data: impl Any + Send + Sync) {
-            if let Some(mapped) = self.data.get_mut(&name) {
-                *mapped = Box::new(data);
-            } else {
-                self.data.insert(name, Box::new(data));
-            }
-        }
+    unsafe {
+        HAS_INIT = true;
+    }
+}
 
-        pub fn add_element_to_static(name: String, data: impl Any + Send + Sync) -> GResult<()> {
-            let mut locked_data = STORAGE_STATIC.lock().map_err(|why| format!("{:?}", why))?;
-            locked_data.set_data_value(name, data);
-            Ok(())
-        }
+impl log::Log for HttpVioletData {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
 
-        pub fn get_data_elemet_static<T: 'static, F>(name: &str, callback: F) -> GResult<()>
-        where
-            F: Fn(&T),
-        {
-            let locked_data = STORAGE_STATIC.lock().map_err(|why| format!("{:?}", why))?;
-            let data = locked_data
-                .data
-                .get(name)
-                .ok_or("Key não possui dados")?
-                .downcast_ref::<T>()
-                .ok_or("Downcast não pode inferir esse tipo")?;
+    fn flush(&self) {
+        todo!()
+    }
 
-            callback(data);
-            Ok(())
-        }
+    fn log(&self, record: &Record) {
+        let cloned_self = self.clone();
+        let pointer_data = (record.level(), record.args().to_string());
+
+        thread::spawn(move || {
+            futures::executor::block_on(async {
+                cloned_self
+                    .send_data("vulcan gay".into(), pointer_data.0.into(), pointer_data.1)
+                    .await
+                    .unwrap();
+            });
+        });
     }
 }
